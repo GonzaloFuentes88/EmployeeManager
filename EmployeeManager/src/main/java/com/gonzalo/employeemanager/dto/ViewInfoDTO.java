94a17f374fdb850e2b85fcfc31be23f0a898878d
GonzaloFuentes88 <fuentesgonzalo88@gmail.com>
2023-11-05 14:02:47 -0300

    Agregamos el objeto dto para el metodo viewInfo

---
+++ EmployeeManager/src/main/java/com/gonzalo/employeemanager/dto/ViewInfoDTO.java
@@ -0,0 +1,20 @@
+package com.gonzalo.employeemanager.dto;
+
+public class ViewInfoDTO {
+	
+	private String identifier;
+	
+	private Boolean isTeam;
+
+	public String getIdentifier() {
+		return identifier;
+	}
+
+	public void setIdentifier(String identifier) {
+		this.identifier = identifier;
+	}
+
+	public Boolean isTeam() {
+		return isTeam;
+	}	
+}
