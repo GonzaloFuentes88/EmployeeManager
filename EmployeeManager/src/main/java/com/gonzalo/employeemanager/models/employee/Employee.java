d1c11071d52439cac599e011f43c1f6b97df66f0
GonzaloFuentes88 <fuentesgonzalo88@gmail.com>
2023-11-05 14:05:47 -0300

    Se agrega el atributo id

--- EmployeeManager/src/main/java/com/gonzalo/employeemanager/models/employee/Employee.java
+++ EmployeeManager/src/main/java/com/gonzalo/employeemanager/models/employee/Employee.java
@@ -8,11 +8,15 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import com.gonzalo.employeemanager.exceptions.InvalidTeamOperationException;
 import com.gonzalo.employeemanager.models.TreeTeam;
 
 public class Employee implements TreeTeam{
 	
 	//Cambio de prueba
+	
+	private Long id;
+	
 	@NotEmpty
 	@Size(min=1, max = 20)
 	private String name;
@@ -31,6 +35,14 @@ public class Employee implements TreeTeam{
 	
 	public static IEmployeeBuilder.Name builder(){
 		return new EmployeeBuilder();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getName() {
@@ -64,33 +76,34 @@ public class Employee implements TreeTeam{
 	public void setSalary(Double salary) {
 		this.salary = salary;
 	}
-
-	
-	
 	
 	@Override
-	public void add() {
-		// TODO Auto-generated method stub
-		
+	public void add(TreeTeam obj) throws InvalidTeamOperationException{
+		throw new InvalidTeamOperationException("It must be added to a team type and not to an employee type");
 	}
 
 	@Override
-	public void remove() {
-		// TODO Auto-generated method stub
-		
+	public void remove(TreeTeam obj) throws InvalidTeamOperationException{
+		throw new InvalidTeamOperationException("It must be removed in an equipment type and not in an employee type");
 	}
 
 	@Override
 	public Boolean isTeam() {
-		// TODO Auto-generated method stub
-		return null;
+		return false;
 	}
 
 	@Override
 	public String viewInfo() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.toString();
 	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", lastName=" + lastName + ", dateOfBirth=" + dateOfBirth
+				+ ", salary=" + salary + "]";
+	}
+	
+	
 	
 	
 	
