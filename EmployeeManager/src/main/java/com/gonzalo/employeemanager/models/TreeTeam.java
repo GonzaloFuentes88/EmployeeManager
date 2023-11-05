a11d673c1259dc452df58791f8099575dfbf998c
GonzaloFuentes88 <fuentesgonzalo88@gmail.com>
2023-11-05 14:07:26 -0300

    Modificacion de los metodos add y remove

--- EmployeeManager/src/main/java/com/gonzalo/employeemanager/models/TreeTeam.java
+++ EmployeeManager/src/main/java/com/gonzalo/employeemanager/models/TreeTeam.java
@@ -1,8 +1,11 @@
 package com.gonzalo.employeemanager.models;
 
+import com.gonzalo.employeemanager.exceptions.InvalidTeamOperationException;
+import com.gonzalo.employeemanager.exceptions.NonExistent;
+
 public interface TreeTeam {
-	public void add();
-	public void remove();
+	public void add(TreeTeam obj) throws InvalidTeamOperationException, NonExistent;
+	public void remove(TreeTeam obj) throws InvalidTeamOperationException, NonExistent;
 	public Boolean isTeam();
 	public String viewInfo();
 }
