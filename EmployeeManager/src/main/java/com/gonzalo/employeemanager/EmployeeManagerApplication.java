e424ece20d56205e19ff53f3b3707720b6e89188
GonzaloFuentes88 <fuentesgonzalo88@gmail.com>
2023-11-06 21:44:34 -0300

    Dependencias y configuracion sencilla para swagger

--- EmployeeManager/src/main/java/com/gonzalo/employeemanager/EmployeeManagerApplication.java
+++ EmployeeManager/src/main/java/com/gonzalo/employeemanager/EmployeeManagerApplication.java
@@ -2,8 +2,13 @@ package com.gonzalo.employeemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @SpringBootApplication
+@EnableWebMvc
 public class EmployeeManagerApplication {
 
 	public static void main(String[] args) {
