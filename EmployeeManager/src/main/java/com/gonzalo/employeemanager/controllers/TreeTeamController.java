7c2b7aa578082ca87550706c281f0fba70433ebb
GonzaloFuentes88 <fuentesgonzalo88@gmail.com>
2023-11-13 21:11:25 -0300

    Se agrega, metodo save,produces y consumes, inyeccion del service

--- EmployeeManager/src/main/java/com/gonzalo/employeemanager/controllers/TreeTeamController.java
+++ EmployeeManager/src/main/java/com/gonzalo/employeemanager/controllers/TreeTeamController.java
@@ -1,19 +1,30 @@
 package com.gonzalo.employeemanager.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gonzalo.employeemanager.dto.SaveTeamInputDTO;
 import com.gonzalo.employeemanager.dto.ViewInfoDTO;
+import com.gonzalo.employeemanager.exceptions.InvalidTeamOperationException;
+import com.gonzalo.employeemanager.exceptions.NonExistent;
 import com.gonzalo.employeemanager.models.TreeTeam;
+import com.gonzalo.employeemanager.models.team.Team;
+import com.gonzalo.employeemanager.service.Service;
 
 @RestController
 @RequestMapping("/treeTeam")
 public class TreeTeamController {
 	
-	@GetMapping("/viewInfo")
+	@Autowired
+	private Service<TreeTeam> service;
+	
+	@GetMapping(value = "/viewInfo", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<TreeTeam> viewInfo(@RequestBody ViewInfoDTO info){
 		TreeTeam obj = null;
 		if(info.isTeam()) {
@@ -23,6 +34,24 @@ public class TreeTeamController {
 		}
 		
 		return ResponseEntity.ok(obj);
+	}
+	
+	@PostMapping(value="/saveTeam",consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Void> saveTeam(@RequestBody SaveTeamInputDTO objIn) throws InvalidTeamOperationException, NonExistent{
+		TreeTeam obj = Team.builder()
+				.teamName(objIn.getTeamName())
+				.tarea(objIn.getTask())
+				.build();
+		
+		try {
+			service.save(obj);
+		} catch (InvalidTeamOperationException e) {
+			throw e;
+		} catch (NonExistent e) {
+			throw e;
+		}
+		
+		return ResponseEntity.created(null).build();
 	}
 
 }
